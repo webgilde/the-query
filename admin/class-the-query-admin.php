d18435f3bc7958ebc16b0364bdf996ff9e1cc1df
webzunft <post@webzunft.de>
2013-11-16 17:18:50 +0100

    added metabox for the query parameters

--- admin/class-the-query-admin.php
+++ admin/class-the-query-admin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * the query
  *
@@ -17,175 +18,175 @@
  */
 class The_Query_Admin {
 
-	/**
-	 * Instance of this class.
-	 *
-	 * @since    1.0.0
-	 *
-	 * @var      object
-	 */
-	protected static $instance = null;
+    /**
+     * Instance of this class.
+     *
+     * @since    1.0.0
+     *
+     * @var      object
+     */
+    protected static $instance = null;
 
-	/**
-	 * Slug of the plugin screen.
-	 *
-	 * @since    1.0.0
-	 *
-	 * @var      string
-	 */
-	protected $plugin_screen_hook_suffix = null;
+    /**
+     * Slug of the plugin screen.
+     *
+     * @since    1.0.0
+     *
+     * @var      string
+     */
+    protected $plugin_screen_hook_suffix = null;
 
-	/**
-	 * Initialize the plugin by loading admin scripts & styles and adding a
-	 * settings page and menu.
-	 *
-	 * @since     1.0.0
-	 */
-	private function __construct() {
+    /**
+     * Initialize the plugin by loading admin scripts & styles and adding a
+     * settings page and menu.
+     *
+     * @since     1.0.0
+     */
+    private function __construct() {
 
-		/*
-		 * Call $plugin_slug from public plugin class.
-		 *
-		 */
-		$plugin = The_Query::get_instance();
-		$this->plugin_slug = $plugin->get_plugin_slug();
+        /*
+         * Call $plugin_slug from public plugin class.
+         *
+         */
+        $plugin = The_Query::get_instance();
+        $this->plugin_slug = $plugin->get_plugin_slug();
 
-		// Load admin style sheet and JavaScript.
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+        // Load admin style sheet and JavaScript.
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_styles'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
 
-		// Add the options page and menu item.
-		add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ) );
+        // Add the options page and menu item.
+        add_action('admin_menu', array($this, 'add_plugin_admin_menu'));
 
-		// Add an action link pointing to the options page.
-		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_slug . '.php' );
-		add_filter( 'plugin_action_links_' . $plugin_basename, array( $this, 'add_action_links' ) );
+        // Add an action link pointing to the options page.
+        $plugin_basename = plugin_basename(plugin_dir_path(__DIR__) . $this->plugin_slug . '.php');
+        add_filter('plugin_action_links_' . $plugin_basename, array($this, 'add_action_links'));
 
-		/*
-		 * Define custom functionality.
-		 *
-		 * Read more about actions and filters:
-		 * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
-		 */
-		// add_action( 'TODO', array( $this, 'action_method_name' ) );
-		// add_filter( 'TODO', array( $this, 'filter_method_name' ) );
+        add_action('admin_init', array($this, 'add_meta_boxes'));
+    }
 
-	}
+    /**
+     * Return an instance of this class.
+     *
+     * @since     1.0.0
+     *
+     * @return    object    A single instance of this class.
+     */
+    public static function get_instance() {
 
-	/**
-	 * Return an instance of this class.
-	 *
-	 * @since     1.0.0
-	 *
-	 * @return    object    A single instance of this class.
-	 */
-	public static function get_instance() {
+        // If the single instance hasn't been set, set it now.
+        if (null == self::$instance) {
+            self::$instance = new self;
+        }
 
-		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance ) {
-			self::$instance = new self;
-		}
+        return self::$instance;
+    }
 
-		return self::$instance;
-	}
+    /**
+     * Register and enqueue admin-specific style sheet.
+     *
+     * @since     1.0.0
+     *
+     * @return    null    Return early if no settings page is registered.
+     */
+    public function enqueue_admin_styles() {
 
-	/**
-	 * Register and enqueue admin-specific style sheet.
-	 *
-	 * @since     1.0.0
-	 *
-	 * @return    null    Return early if no settings page is registered.
-	 */
-	public function enqueue_admin_styles() {
+        if (!isset($this->plugin_screen_hook_suffix)) {
+            return;
+        }
 
-		if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
-			return;
-		}
+        $screen = get_current_screen();
+        if ($this->plugin_screen_hook_suffix == $screen->id) {
+            wp_enqueue_style($this->plugin_slug . '-admin-styles', plugins_url('assets/css/admin.css', __FILE__), array(), The_Query::VERSION);
+        }
+    }
 
-		$screen = get_current_screen();
-		if ( $this->plugin_screen_hook_suffix == $screen->id ) {
-			wp_enqueue_style( $this->plugin_slug .'-admin-styles', plugins_url( 'assets/css/admin.css', __FILE__ ), array(), The_Query::VERSION );
-		}
+    /**
+     * Register and enqueue admin-specific JavaScript.
+     *
+     * @since     1.0.0
+     *
+     * @return    null    Return early if no settings page is registered.
+     */
+    public function enqueue_admin_scripts() {
 
-	}
+        if (!isset($this->plugin_screen_hook_suffix)) {
+            return;
+        }
 
-	/**
-	 * Register and enqueue admin-specific JavaScript.
-	 *
-	 * @since     1.0.0
-	 *
-	 * @return    null    Return early if no settings page is registered.
-	 */
-	public function enqueue_admin_scripts() {
+        $screen = get_current_screen();
+        if ($this->plugin_screen_hook_suffix == $screen->id) {
+            wp_enqueue_script($this->plugin_slug . '-admin-script', plugins_url('assets/js/admin.js', __FILE__), array('jquery'), The_Query::VERSION);
+        }
+    }
 
-		if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
-			return;
-		}
+    /**
+     * Register the administration menu for this plugin into the WordPress Dashboard menu.
+     *
+     * @since    1.0.0
+     */
+    public function add_plugin_admin_menu() {
 
-		$screen = get_current_screen();
-		if ( $this->plugin_screen_hook_suffix == $screen->id ) {
-			wp_enqueue_script( $this->plugin_slug . '-admin-script', plugins_url( 'assets/js/admin.js', __FILE__ ), array( 'jquery' ), The_Query::VERSION );
-		}
+        /*
+         * Add a settings page for this plugin to the Settings menu.
+         */
+        $this->plugin_screen_hook_suffix = add_options_page(
+                __('The_Query settings', $this->plugin_slug), __('The_Query', $this->plugin_slug), 'manage_options', $this->plugin_slug, array($this, 'display_plugin_admin_page')
+        );
+    }
 
-	}
+    /**
+     * Render the settings page for this plugin.
+     *
+     * @since    1.0.0
+     */
+    public function display_plugin_admin_page() {
+        include_once( 'views/admin.php' );
+    }
 
-	/**
-	 * Register the administration menu for this plugin into the WordPress Dashboard menu.
-	 *
-	 * @since    1.0.0
-	 */
-	public function add_plugin_admin_menu() {
+    /**
+     * Add settings action link to the plugins page.
+     *
+     * @since    1.0.0
+     */
+    public function add_action_links($links) {
 
-		/*
-		 * Add a settings page for this plugin to the Settings menu.
-		 */
-		$this->plugin_screen_hook_suffix = add_options_page(
-			__( 'The_Query settings', $this->plugin_slug ),
-			__( 'The_Query', $this->plugin_slug ),
-			'manage_options',
-			$this->plugin_slug,
-			array( $this, 'display_plugin_admin_page' )
-		);
+        return array_merge(
+                        array(
+                    'settings' => '<a href="' . admin_url('options-general.php?page=' . $this->plugin_slug) . '">' . __('Settings', $this->plugin_slug) . '</a>'
+                        ), $links
+        );
+    }
 
-	}
+    /**
+     * Add meta boxes for the query post type
+     *
+     * @since    1.0.0
+     */
+    public function add_meta_boxes() {
+        add_meta_box(
+                'query-parameters-box', __('Query Parameters', $this->plugin_slug), __CLASS__ . '::markup_meta_boxes', The_Query::POST_TYPE_SLUG, 'normal', 'high'
+        );
+    }
 
-	/**
-	 * Render the settings page for this plugin.
-	 *
-	 * @since    1.0.0
-	 */
-	public function display_plugin_admin_page() {
-		include_once( 'views/admin.php' );
-	}
+    /**
+     * load templates for all meta boxes
+     *
+     * @since 1.0.0
+     * @param obj $post
+     * @param array $box
+     */
+    public static function markup_meta_boxes($post, $box) {
+        switch ($box['id']) {
+            case 'query-parameters-box':
+                $view = 'parameters-metabox.php';
+                break;
+        }
 
-	/**
-	 * Add settings action link to the plugins page.
-	 *
-	 * @since    1.0.0
-	 */
-	public function add_action_links( $links ) {
-
-		return array_merge(
-			array(
-				'settings' => '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_slug ) . '">' . __( 'Settings', $this->plugin_slug ) . '</a>'
-			),
-			$links
-		);
-
-	}
-
-	/**
-	 * @since    1.0.0
-	 */
-	public function action_method_name() {
-		// TODO: Define your action hook callback here
-	}
-
-	/**
-	 * @since    1.0.0
-	 */
-	public function filter_method_name() {
-		// TODO: Define your filter hook callback here
-	}
+        $view = plugin_dir_path(__FILE__) . 'views/' . $view;
+        if (is_file($view)) {
+            require_once( $view );
+        }
+    }
 
 }
