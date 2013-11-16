f3ca07cb0f70c1bb23378c641ac4f1738d44fbc3
webzunft <post@webzunft.de>
2013-11-16 17:02:05 +0100

    added custom post type for queries

--- public/class-the-query.php
+++ public/class-the-query.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * the query.
  *
@@ -16,276 +17,316 @@
  * @author  Thomas Maier <thomas.maier@webgilde.com>
  */
 class The_Query {
+    /**
+     * Plugin version, used for cache-busting of style and script file references.
+     *
+     * @since   1.0.0
+     *
+     * @var     string
+     */
 
-	/**
-	 * Plugin version, used for cache-busting of style and script file references.
-	 *
-	 * @since   1.0.0
-	 *
-	 * @var     string
-	 */
-	const VERSION = '1.0.0';
+    const VERSION = '1.0.0';
 
-	/**
-	 * Unique identifier for your plugin.
-	 *
-	 * The variable name is used as the text domain when internationalizing strings
-	 * of text. Its value should match the Text Domain file header in the main
-	 * plugin file.
-	 *
-	 * @since    1.0.0
-	 *
-	 * @var      string
-	 */
-	protected $plugin_slug = 'the-query';
+    /**
+     * post type slug
+     *
+     * @since   1.0.0
+     * @var     string
+     */
+    const POST_TYPE_SLUG = 'the_queries';
 
-	/**
-	 * Instance of this class.
-	 *
-	 * @since    1.0.0
-	 *
-	 * @var      object
-	 */
-	protected static $instance = null;
+    /**
+     * post type name
+     *
+     * @since   1.0.0
+     * @var     string
+     */
+    public $post_type_name = '';
 
-	/**
-	 * Initialize the plugin by setting localization and loading public scripts
-	 * and styles.
-	 *
-	 * @since     1.0.0
-	 */
-	private function __construct() {
+    /**
+     * Unique identifier for your plugin.
+     *
+     * The variable name is used as the text domain when internationalizing strings
+     * of text. Its value should match the Text Domain file header in the main
+     * plugin file.
+     *
+     * @since    1.0.0
+     *
+     * @var      string
+     */
+    protected $plugin_slug = 'the-query';
 
-		// Load plugin text domain
-		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+    /**
+     * Instance of this class.
+     *
+     * @since    1.0.0
+     *
+     * @var      object
+     */
+    protected static $instance = null;
 
-		// Activate plugin when new blog is added
-		add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
+    /**
+     * Initialize the plugin by setting localization and loading public scripts
+     * and styles.
+     *
+     * @since     1.0.0
+     */
+    private function __construct() {
 
-		// Load public-facing style sheet and JavaScript.
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+        // Load plugin text domain
+        add_action('init', array($this, 'load_plugin_textdomain'));
 
-		/* Define custom functionality.
-		 * Refer To http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
-		 */
-		//add_action( 'TODO', array( $this, 'action_method_name' ) );
-		//add_filter( 'TODO', array( $this, 'filter_method_name' ) );
+        // Activate plugin when new blog is added
+        add_action('wpmu_new_blog', array($this, 'activate_new_site'));
 
-	}
+        // Load public-facing style sheet and JavaScript.
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_styles'));
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
 
-	/**
-	 * Return the plugin slug.
-	 *
-	 * @since    1.0.0
-	 *
-	 *@return    Plugin slug variable.
-	 */
-	public function get_plugin_slug() {
-		return $this->plugin_slug;
-	}
+        add_action( 'init', array( $this, 'create_post_type' ) );
+    }
 
-	/**
-	 * Return an instance of this class.
-	 *
-	 * @since     1.0.0
-	 *
-	 * @return    object    A single instance of this class.
-	 */
-	public static function get_instance() {
+    /**
+     * Return the plugin slug.
+     *
+     * @since    1.0.0
+     *
+     * @return    Plugin slug variable.
+     */
+    public function get_plugin_slug() {
+        return $this->plugin_slug;
+    }
 
-		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance ) {
-			self::$instance = new self;
-		}
+    /**
+     * Return an instance of this class.
+     *
+     * @since     1.0.0
+     *
+     * @return    object    A single instance of this class.
+     */
+    public static function get_instance() {
 
-		return self::$instance;
-	}
+        // If the single instance hasn't been set, set it now.
+        if (null == self::$instance) {
+            self::$instance = new self;
+        }
 
-	/**
-	 * Fired when the plugin is activated.
-	 *
-	 * @since    1.0.0
-	 *
-	 * @param    boolean    $network_wide    True if WPMU superadmin uses
-	 *                                       "Network Activate" action, false if
-	 *                                       WPMU is disabled or plugin is
-	 *                                       activated on an individual blog.
-	 */
-	public static function activate( $network_wide ) {
+        return self::$instance;
+    }
 
-		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+    /**
+     * Fired when the plugin is activated.
+     *
+     * @since    1.0.0
+     *
+     * @param    boolean    $network_wide    True if WPMU superadmin uses
+     *                                       "Network Activate" action, false if
+     *                                       WPMU is disabled or plugin is
+     *                                       activated on an individual blog.
+     */
+    public static function activate($network_wide) {
 
-			if ( $network_wide  ) {
+        if (function_exists('is_multisite') && is_multisite()) {
 
-				// Get all blog ids
-				$blog_ids = self::get_blog_ids();
+            if ($network_wide) {
 
-				foreach ( $blog_ids as $blog_id ) {
+                // Get all blog ids
+                $blog_ids = self::get_blog_ids();
 
-					switch_to_blog( $blog_id );
-					self::single_activate();
-				}
+                foreach ($blog_ids as $blog_id) {
 
-				restore_current_blog();
+                    switch_to_blog($blog_id);
+                    self::single_activate();
+                }
 
-			} else {
-				self::single_activate();
-			}
+                restore_current_blog();
+            } else {
+                self::single_activate();
+            }
+        } else {
+            self::single_activate();
+        }
+    }
 
-		} else {
-			self::single_activate();
-		}
+    /**
+     * Fired when the plugin is deactivated.
+     *
+     * @since    1.0.0
+     *
+     * @param    boolean    $network_wide    True if WPMU superadmin uses
+     *                                       "Network Deactivate" action, false if
+     *                                       WPMU is disabled or plugin is
+     *                                       deactivated on an individual blog.
+     */
+    public static function deactivate($network_wide) {
 
-	}
+        if (function_exists('is_multisite') && is_multisite()) {
 
-	/**
-	 * Fired when the plugin is deactivated.
-	 *
-	 * @since    1.0.0
-	 *
-	 * @param    boolean    $network_wide    True if WPMU superadmin uses
-	 *                                       "Network Deactivate" action, false if
-	 *                                       WPMU is disabled or plugin is
-	 *                                       deactivated on an individual blog.
-	 */
-	public static function deactivate( $network_wide ) {
+            if ($network_wide) {
 
-		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+                // Get all blog ids
+                $blog_ids = self::get_blog_ids();
 
-			if ( $network_wide ) {
+                foreach ($blog_ids as $blog_id) {
 
-				// Get all blog ids
-				$blog_ids = self::get_blog_ids();
+                    switch_to_blog($blog_id);
+                    self::single_deactivate();
+                }
 
-				foreach ( $blog_ids as $blog_id ) {
+                restore_current_blog();
+            } else {
+                self::single_deactivate();
+            }
+        } else {
+            self::single_deactivate();
+        }
+    }
 
-					switch_to_blog( $blog_id );
-					self::single_deactivate();
+    /**
+     * Fired when a new site is activated with a WPMU environment.
+     *
+     * @since    1.0.0
+     *
+     * @param    int    $blog_id    ID of the new blog.
+     */
+    public function activate_new_site($blog_id) {
 
-				}
+        if (1 !== did_action('wpmu_new_blog')) {
+            return;
+        }
 
-				restore_current_blog();
+        switch_to_blog($blog_id);
+        self::single_activate();
+        restore_current_blog();
+    }
 
-			} else {
-				self::single_deactivate();
-			}
+    /**
+     * Get all blog ids of blogs in the current network that are:
+     * - not archived
+     * - not spam
+     * - not deleted
+     *
+     * @since    1.0.0
+     *
+     * @return   array|false    The blog ids, false if no matches.
+     */
+    private static function get_blog_ids() {
 
-		} else {
-			self::single_deactivate();
-		}
+        global $wpdb;
 
-	}
-
-	/**
-	 * Fired when a new site is activated with a WPMU environment.
-	 *
-	 * @since    1.0.0
-	 *
-	 * @param    int    $blog_id    ID of the new blog.
-	 */
-	public function activate_new_site( $blog_id ) {
-
-		if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
-			return;
-		}
-
-		switch_to_blog( $blog_id );
-		self::single_activate();
-		restore_current_blog();
-
-	}
-
-	/**
-	 * Get all blog ids of blogs in the current network that are:
-	 * - not archived
-	 * - not spam
-	 * - not deleted
-	 *
-	 * @since    1.0.0
-	 *
-	 * @return   array|false    The blog ids, false if no matches.
-	 */
-	private static function get_blog_ids() {
-
-		global $wpdb;
-
-		// get an array of blog ids
-		$sql = "SELECT blog_id FROM $wpdb->blogs
+        // get an array of blog ids
+        $sql = "SELECT blog_id FROM $wpdb->blogs
 			WHERE archived = '0' AND spam = '0'
 			AND deleted = '0'";
 
-		return $wpdb->get_col( $sql );
+        return $wpdb->get_col($sql);
+    }
 
-	}
+    /**
+     * Fired for each blog when the plugin is activated.
+     *
+     * @since    1.0.0
+     */
+    private static function single_activate() {
+        // TODO: Define activation functionality here
+    }
 
-	/**
-	 * Fired for each blog when the plugin is activated.
-	 *
-	 * @since    1.0.0
-	 */
-	private static function single_activate() {
-		// TODO: Define activation functionality here
-	}
+    /**
+     * Fired for each blog when the plugin is deactivated.
+     *
+     * @since    1.0.0
+     */
+    private static function single_deactivate() {
+        // TODO: Define deactivation functionality here
+    }
 
-	/**
-	 * Fired for each blog when the plugin is deactivated.
-	 *
-	 * @since    1.0.0
-	 */
-	private static function single_deactivate() {
-		// TODO: Define deactivation functionality here
-	}
+    /**
+     * Load the plugin text domain for translation.
+     *
+     * @since    1.0.0
+     */
+    public function load_plugin_textdomain() {
 
-	/**
-	 * Load the plugin text domain for translation.
-	 *
-	 * @since    1.0.0
-	 */
-	public function load_plugin_textdomain() {
+        $domain = $this->plugin_slug;
+        $locale = apply_filters('plugin_locale', get_locale(), $domain);
 
-		$domain = $this->plugin_slug;
-		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+        load_textdomain($domain, trailingslashit(WP_LANG_DIR) . $domain . '/' . $domain . '-' . $locale . '.mo');
+        load_plugin_textdomain($domain, FALSE, basename(plugin_dir_path(dirname(__FILE__))) . 'languages/');
+    }
 
-		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
-		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . 'languages/' );
+    /**
+     * Register and enqueue public-facing style sheet.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles() {
+        wp_enqueue_style($this->plugin_slug . '-plugin-styles', plugins_url('assets/css/public.css', __FILE__), array(), self::VERSION);
+    }
 
-	}
+    /**
+     * Register and enqueues public-facing JavaScript files.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts() {
+        wp_enqueue_script($this->plugin_slug . '-plugin-script', plugins_url('assets/js/public.js', __FILE__), array('jquery'), self::VERSION);
+    }
 
-	/**
-	 * Register and enqueue public-facing style sheet.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-		wp_enqueue_style( $this->plugin_slug . '-plugin-styles', plugins_url( 'assets/css/public.css', __FILE__ ), array(), self::VERSION );
-	}
+    /**
+     * Registers the custom post type
+     *
+     * @since 1.0.0
+     */
+    public function create_post_type() {
+        if (did_action('init') !== 1) {
+            return;
+        }
 
-	/**
-	 * Register and enqueues public-facing JavaScript files.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-		wp_enqueue_script( $this->plugin_slug . '-plugin-script', plugins_url( 'assets/js/public.js', __FILE__ ), array( 'jquery' ), self::VERSION );
-	}
+        if (!post_type_exists(self::POST_TYPE_SLUG)) {
+            $post_type_params = $this->get_post_type_params();
+            $post_type = register_post_type(self::POST_TYPE_SLUG, $post_type_params);
+        }
+    }
 
-	/**
-	 *
-	 * @since    1.0.0
-	 */
-	public function action_method_name() {
-		// TODO: Define your action hook callback here
-	}
+    /**
+     * Defines the parameters for the custom post type
+     *
+     * @since 1.0.0
+     *
+     * @return array
+     */
+    protected function get_post_type_params() {
+        $labels = array(
+            'name' => __('Queries', $this->plugin_slug),
+            'singular_name' => __('The Query', $this->plugin_slug),
+            'add_new' => 'Add New',
+            'add_new_item' => __('Add New Query', $this->plugin_slug),
+            'edit' => __('Edit', $this->plugin_slug),
+            'edit_item' => __('Edit Query', $this->plugin_slug),
+            'new_item' => __('New Query', $this->plugin_slug),
+            'view' => __('View', $this->plugin_slug),
+            'view_item' => __('View The Query', $this->plugin_slug),
+            'search_items' => __('Search Queries', $this->plugin_slug),
+            'not_found' => __('No Queries found', $this->plugin_slug),
+            'not_found_in_trash' => __('No Queries found in Trash', $this->plugin_slug),
+            'parent' => __('Parent Query', $this->plugin_slug),
+        );
 
-	/**
-	 *
-	 * @since    1.0.0
-	 */
-	public function filter_method_name() {
-		// TODO: Define your filter hook callback here
-	}
+        $post_type_params = array(
+            'labels' => $labels,
+            'singular_label' => __('The Query', $this->plugin_slug),
+            'public' => false,
+            'show_ui' => true,
+            'menu_position' => 100,
+            'hierarchical' => false,
+            'capability_type' => 'page',
+            'has_archive' => false,
+            'rewrite' => array('slug' => $this->plugin_slug),
+            'query_var' => true,
+            'supports' => array('title')
+        );
 
-
+        return apply_filters('the-query-post-type-params', $post_type_params);
+    }
 
 }
